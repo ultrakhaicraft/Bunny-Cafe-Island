e28634e375d803d2f7e6765a3bb8f00048c73eaf
Phan Quang Khai <khaipqse183063@fpt.edu.vn>
2024-08-20 14:22:51 +0700

    Fixing backend file group

--- Back end/BunnyIslandCafe/src/main/java/com/BunnyIslandCafe/BunnyIslandCafeApplication.java
+++ Back end/BunnyIslandCafe/src/main/java/com/BunnyIslandCafe/BunnyIslandCafeApplication.java
@@ -1,4 +1,4 @@
-package com.example.Bunny.Island.Cafe;
+package com.BunnyIslandCafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Back end/BunnyIslandCafe/src/test/java/com/BunnyIslandCafe/BunnyIslandCafeApplicationTests.java
+++ Back end/BunnyIslandCafe/src/test/java/com/BunnyIslandCafe/BunnyIslandCafeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Bunny.Island.Cafe;
+package com.BunnyIslandCafe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
