de03bacf829d1959ddc1f1e38fa5a8653e62d789
Phan Quang Khai <khaipqse183063@fpt.edu.vn>
2024-08-20 14:06:39 +0700

    Add Backend component

---
+++ Back end/Bunny-Island-Cafe/src/main/java/com/example/Bunny/Island/Cafe/BunnyIslandCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bunny.Island.Cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunnyIslandCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunnyIslandCafeApplication.class, args);
+	}
+
+}
---
+++ Back end/Bunny-Island-Cafe/src/test/java/com/example/Bunny/Island/Cafe/BunnyIslandCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bunny.Island.Cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunnyIslandCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
